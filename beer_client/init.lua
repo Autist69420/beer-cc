77f25efa462b702a3927c78d0a79ac3cd7567465
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2022-05-30 17:30:28 +0200

    fuck you, lua

--- beer_client/init.lua
+++ beer_client/init.lua
@@ -1,28 +1,23 @@
-local modem = peripheral.getName("modem")
 local modem_side = arg[1]
 
 local net = rednet.open(modem_side)
 local inventory = require("inventory")
 local json = require("../json")
 
-if modem_side then
-    net.send(0, "client_request")
+net.send(0, "client_request")
 
-    while true do
-        local barrels = inventory.getBarrels()
-        for i = 1, #barrels do
-            local barrel_id = barrels[i]:sub(18)
-            local inventory = inventory.getInventoryOfBarrelId(barrel_id)
-            local msg = {
-                client_id = os.getComputerID(),
-                barrel_id = barrel_id,
-                inventory = inventory
-            }
-            local msg_json = json.encode(msg)
-            net.send(0, "JSON_BEER_SEND:" .. msg_json)
-            print("sent")
-        end
+while true do
+    local barrels = inventory.getBarrels()
+    for i = 1, #barrels do
+        local barrel_id = barrels[i]:sub(18)
+        local inventory = inventory.getInventoryOfBarrelId(barrel_id)
+        local msg = {
+            client_id = os.getComputerID(),
+            barrel_id = barrel_id,
+            inventory = inventory
+        }
+        local msg_json = json.encode(msg)
+        net.send(0, "JSON_BEER_SEND:" .. msg_json)
+        print("sent")
     end
-else
-    error("Invalid side or no side given")
 end

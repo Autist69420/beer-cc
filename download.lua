cffda4046699daafa1555667f9d328b8fe821dba
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2022-05-30 15:20:02 +0200

    Add download file

---
+++ download.lua
@@ -0,0 +1,35 @@
+-- Downloads the files, lmao
+
+local to_download = arg[1]
+
+local CLIENT_URL = "https://api.github.com/repos/Autist69420/beer-cc/contents/beer_client"
+local SERVER_URL = "https://api.github.com/repos/Autist69420/beer-cc/contents/beer_server"
+
+if to_download == "client" then
+    -- Download the client files
+    local ok, err = http.checkURL("https://api.github.com/repos/Autist69420/beer-cc/contents/beer_client")
+    if not ok then
+        print("Error: " .. err)
+        return
+    end
+
+    local client_files = http.get(CLIENT_URL)
+    local client_files_json = textutils.unserialize(client_files.readAll())
+    for i = 1, #client_files_json do
+        local file_name = client_files_json[i].name
+        local file_url = client_files_json[i].download_url
+        local file_content = http.get(file_url)
+        local file_content_str = file_content.readAll()
+        local file_path = "beer_client/" .. file_name
+        local file_handle = fs.open(file_path, "w")
+        file_handle.write(file_content_str)
+        file_handle.close()
+    end
+
+    print("Downloaded client.")
+else if to_download == "server" then
+    -- Download the server files
+else
+    error("Invalid argument", 0)
+    exit()
+end

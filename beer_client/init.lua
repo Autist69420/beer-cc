8a5b8fd5f5422571f5b300e76aa737407db67308
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2022-05-30 15:51:36 +0200

    Client and server changes

--- beer_client/init.lua
+++ beer_client/init.lua
@@ -12,4 +12,18 @@ while true do
     if channel == 80 then
         print("Received a reply: " .. tostring(message))
     end
+
+    local barrels = inventory.getBarrels()
+    for i = 1 in #barrels do
+        local barrel_id = barrels[i]:sub(11)
+        local inventory = inventory.getInventoryOfBarrelId(barrel_id)
+        local msg = {
+            client_id = 1,
+            barrel_id = barrel_id,
+            inventory = inventory
+        }
+        local msg_json = json.encode(msg)
+        modem.transmit(81, 80, msg_json)
+    end
+    end
 end
---
+++ beer_server/init.lua
@@ -0,0 +1,33 @@
+local modem = peripheral.find("modem") or error("No modem attached", 0)
+local json = require("../json")
+
+local amount_of_clients = 0
+
+modem.open(81)
+print("Opened to port 81")
+
+while true do
+    local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+
+    if channel == 81 then
+        if tostring(message) == "client_request" then
+            amount_of_clients = amount_of_clients + 1
+            print("Client connected, amount of clients: " .. amount_of_clients)
+        else
+            local msg = tostring(message)
+            local msg_json = json.decode(msg)
+            
+            local client_id = msg_json.client_id
+            local barrel_id = msg_json.barrel_id
+            local inventory = msg_json.inventory
+
+            -- loop trough the inventory and check if the client has the item
+            for i = 1, #inventory do
+                local item_name = inventory[i].name
+                local item_amount = inventory[i].amount
+
+                print(item_name, item_amount)
+            end
+        end
+    end
+end

a45cee2b000041e116b3debc51fd513fa829eed6
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2022-05-31 14:57:11 +0200

    Rewrite client and server

--- beer_client/client.lua
+++ beer_client/client.lua
@@ -7,17 +7,42 @@ local json = require("../json")
 rednet.send(0, "client_request")
 
 while true do
-    local barrels = inventory.getBarrels()
-    for i = 1, #barrels do
-        local barrel_id = barrels[i]:gsub("minecraft:barrel_", "")
-        local inventory = inventory.getInventoryOfBarrelId(barrel_id)
-        local msg = {
-            client_id = os.getComputerID(),
-            barrel_id = barrel_id,
-            inventory = inventory
+    local beerChests = {}
+    local chests = inventory.getChests()
+    -- for i = 1, #barrels do
+    --     local barrel_id = barrels[i]:gsub("minecraft:barrel_", "")
+    --     local inventory = inventory.getInventoryOfBarrelId(barrel_id)
+    --     local msg = {
+    --         client_id = os.getComputerID(),
+    --         barrel_id = barrel_id,
+    --         inventory = inventory
+    --     }
+    --     local msg_json = json.encode(msg)
+    --     rednet.send(0, "JSON_BEER_SEND:" .. msg_json)
+    --     print("sent")
+    -- end
+
+    for _, chest in pairs(chests) do 
+        -- inventory.getItemDetailByChestName(name, slot)
+        local inv = inventory.getInventoryByChestName(chest)
+        local slots = #inv
+        local name = inventory.getItemDetailByChestName(chest, 1).displayName
+        local count = 0
+        
+        for i, _ in pairs(inv) do
+            local detail = inventory.getItemDetailByChestName(chest, i)
+            count = count + detail.count
+        end
+        
+        -- add to beerChests
+        beerChests[#beerChests + 1] = {
+            name = name,
+            count = count,
+            slots = slots
         }
-        local msg_json = json.encode(msg)
-        rednet.send(0, "JSON_BEER_SEND:" .. msg_json)
-        print("sent")
     end
+    
+    rednet.send(0, json.encode(beerChests), "BEER")
+    -- sleep for 5 second, we do not want it to send 2735429374 times a second
+    os.sleep(5)
 end
--- beer_server/server.lua
+++ beer_server/server.lua
@@ -15,48 +15,26 @@ local json = require("../json")
 local screen = peripheral.find("monitor") or error("No monitor attached", 0)
 local x, y = screen.getSize()
 
-local tab_bar = surface.create(x, 2, " ", colors.gray, colors.white)
+local tab_bar = surface.create(x, 1, " ", colors.gray, colors.white)
 local main_surf = surface.create(x, y, " ", colors.blue, colors.white)
 
 tab_bar:drawText(1, 1, "Beer Server")
 main_surf:drawText(1, 1, "Welcome to le epic beer server")
 
-main_surf:drawText(1, 3, "- Current beer:")
-main_surf:drawText(2, 4, "- Miner's Pale Ale: 5")
-
-tab_bar:render(screen, 1, 1)
-main_surf:render(screen, 1, 3)
-
---[[
 while true do
-    local id, message = rednet.receive(nil, 5)
+    main_surf:drawText(1, 3, "- Current beer:")
 
-    if tostring(message) == "client_request" then
-        amount_of_clients = amount_of_clients + 1
-        print("Client connected, amount of clients: " .. amount_of_clients)
-    elseif string.find(tostring(message), "JSON_BEER_SEND") then
-        local msg = string.gsub(tostring(message), "JSON_BEER_SEND:", "")
-        local msg_json = json.decode(msg)
-        
-        local client_id = msg_json.client_id
-        local barrel_id = msg_json.barrel_id
-        local inventory = msg_json.inventory
-        
-        local same_items_count = 0
-        local same_items_name = ""
-
-        -- loop trough the inventory and check if the client has the item
-        for i = 1, #inventory do
-            local item_name = inventory[i].name
-            local item_amount = inventory[i].count
-
-            same_items_count = same_items_count + item_amount
-            same_items_name = item_name
-        end
-
-        beer_wind:addItem(same_items_name, same_items_count)
+    local msg = rednet.receive("BEER")
+    local beerChests = json.decode(msg)
+    
+    for i, chest in pairs(beerChests) do
+        local name = chest.name
+        local count = chest.count
+        local slots = chest.slots
+        main_surf:drawText(2, 2 + i, "- " .. name .. ": " .. count .. " (" .. slots .. " slots)")
     end
 
-    beer_wind:update()
+    tab_bar:render(screen, 1, 1)
+    main_surf:render(screen, 1, 3)
 end
-]]
+
